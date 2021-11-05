de3cbb927d0d4360b132d2f747340321affed800
Ossama Nbadou <nba.ossama@gmail.com>
2021-11-05 16:40:28 +0100

    Ajout de C3

---
+++ TP2-Genie Logiciel/src/ma/ac/esi/C3.java
@@ -0,0 +1,12 @@
+package ma.ac.esi;
+
+public class C3 {
+	public static void main(String[] args) {
+		for (int i = 1; i < 8; i++) {
+			if (i%2==0) {
+				System.out.println(i);
+			}
+			}
+		}
+
+}
